32a6216e79950dd5265ba3c32873d3440035d7cd
David Chen <d0123456789g@gmail.com>
2018-09-08 13:46:07 +0800

    tmp.sh - fix search method
    Signed-off-by: David Chen <d0123456789g@gmail.com>

--- tmp.sh
+++ tmp.sh
@@ -1,1 +1,6 @@
-lynx -dump -listonly -nonumbers https://www.youtube.com/results?search_query=heldeep+radio+%23 | grep watch |sort -u |grep -v list
+ : "${*// /+}"
+linkYouTubeSearch1="https://www.youtube.com/results?search_query=${_}"
+
+if [ ! -z "${linkYouTubeSearch1}" ]; then
+    lynx -dump -listonly -nonumbers ${linkYouTubeSearch1} | grep watch |sort -u |grep -v list >list.list
+fi
